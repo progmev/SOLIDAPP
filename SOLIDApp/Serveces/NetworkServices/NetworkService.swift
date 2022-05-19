073d175bc9c4f84eeac8136007c7a6e15c63d647
Martynov Evgeny <martynov@wimix.by>
2022-05-19 18:24:50 +0300

    Day 1 fin

--- SOLIDApp/Serveces/NetworkServices/NetworkService.swift
+++ SOLIDApp/Serveces/NetworkServices/NetworkService.swift
@@ -18,18 +18,19 @@ protocol NetworkServiceProtocol {
 
 final class NetworkService: NetworkServiceProtocol {
     
-    func request(urlString: String,
-                 completion: @escaping (Data?, Error?) -> Void)
-    {
-        guard let url = URL(string: urlString) else {
-            return
-        }
+    public func request(urlString: String, completion: @escaping (Data?, Error?) -> Void) {
+        guard let url = URL(string: urlString) else { return }
         let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        // создание URLSessionDataTask тоже вынесем в отдельную ф-ю
+        let task = createDataTask(from: request, completion: completion)
+        task.resume()
+    }
+
+    private func createDataTask(from requst: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: requst, completionHandler: { (data, response, error) in
             DispatchQueue.main.async {
                 completion(data, error)
             }
-        }
-        task.resume()
+        })
     }
 }
